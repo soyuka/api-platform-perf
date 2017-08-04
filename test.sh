f7881efb2dfa59673670f3a04b493bc050b32e20
abluchet <abluchet@ds-restauration.com>
2017-08-04 14:27:39 +0200

    more

--- test.sh
+++ test.sh
@@ -6,3 +6,14 @@ echo "no cache"
 curl -w "Time total: %{time_total}" -o /dev/null -s "http://localhost:8001/foos.jsonld?pagination=false"
 echo "cache"
 curl -w "Time total: %{time_total}" -o /dev/null -s "http://localhost:8001/foos.jsonld?pagination=false"
+echo "Let's patch this shit"
+cd vendor/api-platform/core
+patch -p1 < ../../../perf.patch
+cd ../../../
+rm -r var/cache/*
+bin/console server:stop
+bin/console server:start
+echo "no cache"
+curl -w "Time total: %{time_total}" -o /dev/null -s "http://localhost:8001/foos.jsonld?pagination=false"
+echo "cache"
+curl -w "Time total: %{time_total}" -o /dev/null -s "http://localhost:8001/foos.jsonld?pagination=false"
